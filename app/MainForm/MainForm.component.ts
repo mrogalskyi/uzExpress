7d729819b6b13c242f8a34473114137e2b86efe8
Anna Polonska <Anna Polonska>
2016-12-11 22:05:23 +0200

    Add tslint

--- app/MainForm/MainForm.component.ts
+++ app/MainForm/MainForm.component.ts
@@ -1,33 +1,33 @@
-import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from "@angular/core";
 import {
     FormBuilder,
     FormGroup
-} from '@angular/forms';
-import {Observable} from 'rxjs';
-import { Router, ActivatedRoute, Params } from '@angular/router';
+} from "@angular/forms";
+import { Observable } from "rxjs";
+import { Router, ActivatedRoute, Params } from "@angular/router";
 
-import { RequestParameters } from './../Models/RequestParameters';
-import { StationService } from '../Services/StationService';
-import {Station} from '../Models/Station';
+import { RequestParameters } from "./../Models/RequestParameters";
+import { StationService } from "../Services/StationService";
+import { Station } from "../Models/Station";
 
 
 @Component({
-    selector: 'main-form',
-    template: require('./MainForm.component.html')
+    selector: "main-form",
+    template: require("./MainForm.component.html")
 })
 export class MainFromComponent implements OnInit {
     @Output() submitCall = new EventEmitter<RequestParameters>();
-    mainForm: FormGroup
+    mainForm: FormGroup;
     stations: Station[];
 
     ngOnInit() {
         this.route.queryParams.subscribe((params: Params) => {
-            this.mainForm.controls['from'].setValue(params['fromStation'] || '');
-            this.mainForm.controls['to'].setValue(params['toStation'] || '');
-            this.mainForm.controls['when'].setValue(params['departureDate'] || '');
-            this.mainForm.controls['time'].setValue(params['departureTime'] || '');
-            this.mainForm.controls['sessid'].setValue(params['sessId'] || '');
-            this.mainForm.controls['gvToken'].setValue(params['gvToken'] || '');
+            this.mainForm.controls["from"].setValue(params["fromStation"] || "");
+            this.mainForm.controls["to"].setValue(params["toStation"] || "");
+            this.mainForm.controls["when"].setValue(params["departureDate"] || "");
+            this.mainForm.controls["time"].setValue(params["departureTime"] || "");
+            this.mainForm.controls["sessid"].setValue(params["sessId"] || "");
+            this.mainForm.controls["gvToken"].setValue(params["gvToken"] || "");
         });
     }
 
@@ -37,25 +37,25 @@ export class MainFromComponent implements OnInit {
         private stationService: StationService,
         fb: FormBuilder
     ) {
-        this.stationService.getStations().subscribe( stations => this.stations = stations)
+        this.stationService.getStations().subscribe( stations => this.stations = stations);
         this.mainForm = fb.group({
-            from: [''],
-            to: [''],
-            when: [''],
-            time: [''],
-            sessid: [''],
-            gvToken: ['']
+            from: [""],
+            to: [""],
+            when: [""],
+            time: [""],
+            sessid: [""],
+            gvToken: [""]
         });
     }
 
     submitClick(values: any) {
 
-        var params = new RequestParameters(values);
-        this.router.navigate(['/search'], { queryParams: params });
+        let params = new RequestParameters(values);
+        this.router.navigate(["/search"], { queryParams: params });
         this.onSubmit(params);
-        Observable.timer(0, 1000).subscribe((value)=> {
+        Observable.timer(0, 1000).subscribe((value) => {
             console.log(value);
-            if(value % 10 == 0) {
+            if (value % 10 === 0) {
                 this.onSubmit(params);
             }
         });
@@ -65,6 +65,4 @@ export class MainFromComponent implements OnInit {
     onSubmit(params: RequestParameters) {
         this.submitCall.next(params);
     }
-
-    
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,32 @@
-var webpack = require('webpack');
-var HtmlWebpackPlugin = require('html-webpack-plugin');
-var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var webpack = require("webpack");
+var HtmlWebpackPlugin = require("html-webpack-plugin");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
     entry: "./app/main.ts",
 
     output: {
-        filename: 'main.js',
-        path: './dist'
+        filename: "main.js",
+        path: "./dist"
     },
     resolve: {
-        extensions: ['', '.ts', '.js', '.html']
+        extensions: ["", ".ts", ".js", ".html"]
     },
     module: {
+        preLoaders: [
+            {
+                test: /\.ts$/,
+                loader: "tslint-loader"
+            }
+        ],
         loaders: [
             {
                 test: /\.ts$/,
-                loaders: ['awesome-typescript-loader', 'angular2-template-loader']
+                loaders: ["awesome-typescript-loader", "angular2-template-loader"]
             },
             {
                 test: /\.html$/,
-                loader: 'raw'
+                loader: "raw"
             },
             {
                 test: /\.css$/,
@@ -28,17 +34,17 @@ module.exports = {
             },
             {
                 test: /\.(png|jpe?g|gif|svg|woff|woff2|ttf|eot|ico)$/,
-                loader: 'file?name=assets/[name].[hash].[ext]'
+                loader: "file?name=assets/[name].[hash].[ext]"
             },
         ]
     },
-    devServer: { 
-        inline: true 
+    devServer: {
+        inline: true
     },
     plugins: [
         new HtmlWebpackPlugin({
-            template: './app/index.html'
+            template: "./app/index.html"
         })
     ]
 
-}
+};
