b11b307b832ac8d2dc92c2c9f840d92e93dbb158
m.rogalsky <m.rogalsky@gmail.com>
2016-12-12 23:28:07 +0200

    Add continiously search

--- app/Search/Search.component.ts
+++ app/Search/Search.component.ts
@@ -1,11 +1,13 @@
 import { Component } from "@angular/core";
 import { RequestParameters } from "../Models/RequestParameters";
 import { TicketsService } from "../Services/TicketsService";
+import { Observable, Subject } from "rxjs";
+import { Train } from "../Models/Train";
 @Component({
   selector: "uz-search",
   template: `
     <h1>Tickets helper</h1>
-    <uz-main-form (startSearch)="searchStart($event)"></uz-main-form>
+    <uz-main-form (startSearch)="searchStart($event)" (stopSearch)="searchStop()"></uz-main-form>
     {{error}}
     <train-list [trains]="trains"></train-list>
     `
@@ -13,12 +15,17 @@ import { TicketsService } from "../Services/TicketsService";
 export class SearchComponent {
   trains: any[];
   error: string;
+  search: Observable<Train[]>;
+  stop: Subject<boolean> = new Subject<boolean>();
   constructor(public ticketsService: TicketsService) {
   }
   searchStart(params: RequestParameters): void {
     this.error = "";
     this.trains = [];
-    this.ticketsService.getTrainsWithTickets(params)
-    .subscribe((trains) => this.trains = trains, (err) => this.error = err);
+    this.search = this.ticketsService.getTrainsWithTicketsStream(params).takeUntil(this.stop);
+    this.search.subscribe((trains) => this.trains = trains, (err) => this.error = err);
+  }
+   searchStop(): void {
+     this.stop.next(true);
   }
 }
--- app/Services/TicketsService.ts
+++ app/Services/TicketsService.ts
@@ -1,26 +1,30 @@
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
-import { RequestParameters } from '../Models/RequestParameters';
-import { Http, Response } from '@angular/http';
-import * as moment from 'moment';
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { RequestParameters } from "../Models/RequestParameters";
+import { Http, Response } from "@angular/http";
+import * as moment from "moment";
+import { IntervalObservable } from "rxjs/observable/IntervalObservable";
 
-import {Train} from '../Models/Train';
+import { Train } from "../Models/Train";
 
 @Injectable()
 export class TicketsService {
     constructor(private http: Http) {
 
     }
-    getTrainsWithTickets(params: RequestParameters): Observable<Train[]> {
-        let trains = this.http.post(`http://localhost:3000/tickets`, Object.assign({},params,{departureDate: moment(params.departureDate).format('DD.MM.YYYY')}))
-            .map((res) => res.json())
+    getTrainsWithTicketsStream(params: RequestParameters): Observable<Train[]> {
+        let formatedParams = Object.assign({}, params, {departureDate: moment(params.departureDate).format("DD.MM.YYYY")});
+        let trainStream = IntervalObservable.create(10000)
+            .startWith(0)
+            .flatMap(() => this.http.post(`http://localhost:3000/tickets`, formatedParams))
+            .map(res => res.json())
             .map((res) => {
-                if(res.value && Array.isArray(res.value)) {
+                if (res.value && Array.isArray(res.value)) {
                     console.log(res.value);
-                    return res.value.map((val) => new Train(val))
+                    return res.value.map((val) => new Train(val));
                 }
-                throw new Error(res.value || 'Server error')
+                throw new Error(res.value || "Server error");
             });
-        return trains;
+        return trainStream;
     }
 }
